7077e74daf5a1b3828fbe4bc1ce109ad0237d3a3
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-11 10:27:02 +0530

    About Section Made

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,9 @@ import '../styles/about.scss';
 const About = () => {
     return(
         <div>
-            <h3 className="heading"> About Me : </h3>
-            <h1>urbgekkrb</h1>
-            <h6>jnbeknbwek</h6>
+            <h3 className="heading"> <a className="number-about">01. </a>About Me </h3>
+            <h4 className="about-me"> Web Developer || Tech Enthusiast || Sophomore@IIITV || I build stuff 💪🏼✅ </h4>
+            <h4>jnbeknbwek</h4>
         </div>
     );
 }
