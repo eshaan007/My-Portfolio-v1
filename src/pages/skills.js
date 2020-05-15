a16b29348d525dc8d54ed2e72c0e602cb8d8bc8a
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-16 04:50:39 +0530

    Updated Skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -17,6 +17,12 @@ import postman from '../images/postman.png';
 import python3 from '../images/python3.png';
 import react from '../images/react.png';
 import sass from '../images/sass.png';
+import expressjs from '../images/expressjs.png';
+import netlify from '../images/netlify.png';
+import vscode from '../images/vscode.png';
+import figma from '../images/figma.png';
+import mysql from '../images/mysql.png';
+import mongodb from '../images/mongodb.png';
 
 import '../styles/skills.scss';
 
@@ -24,7 +30,7 @@ const Skills = () => {
     return(
         <div>
             <h3 className="heading"> <a className="number-about">02. </a> My Skill - Set </h3>
-            <h4 className="about-me-bold1"> These are the technologies I have experience working with :</h4>
+            <h4 className="about-me-bold1"> Some technologies I have experience working with :</h4>
             
             <div id="no-mob" className="logo-container">
                 
@@ -34,21 +40,25 @@ const Skills = () => {
                 <img className="logo" src={js} alt="js"></img>
                 <img className="logo" src={react} alt="react"></img>
                 <img className="logo" src={bootstrap4} alt="bootstrap"></img>
+                <img className="logo" src={jquery} alt="jquery"></img> 
                 <img className="logo" src={github} alt="github"></img>
-                
-                
-                
-                <img className="logo" src={android} alt="android"></img>
+                <img className="logo" src={java} alt="java"></img>
+                <img className="logo" src={python3} alt="py"></img>
                 <img className="logo" src={C} alt="C"></img>
                 <img className="logo" src={Cpp} alt="C++"></img>
-                <img className="logo" src={firebase} alt="firebase"></img>
-                <img className="logo" src={java} alt="java"></img>
-                <img className="logo" src={jquery} alt="jquery"></img>
-                <img className="logo" src={linux} alt="linux"></img>
                 <img className="logo" src={nodejs} alt="node"></img>
+                <img className="logo1" src={mongodb} alt="mongodb"></img>
                 <img className="logo" src={npm} alt="npm"></img>
+                <img className="logo" src={expressjs} alt="express"></img>
                 <img className="logo" src={postman} alt="postman"></img>
-                <img className="logo" src={python3} alt="py"></img>
+                <img className="logo" src={mysql} alt="mysql"></img>
+                <img className="logo" src={android} alt="android"></img>
+                <img className="logo" src={firebase} alt="firebase"></img>
+                <img className="logo" src={linux} alt="linux"></img>
+                <img className="logo" src={vscode} alt="visual studio code"></img>
+                <img className="logo" src={figma} alt="figma"></img>
+                <img className="logo" src={netlify} alt="netlify"></img>
+                
             </div>
         </div>
     )
