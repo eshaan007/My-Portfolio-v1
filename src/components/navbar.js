7c96fe74c9f5cb37e8fec580bf3889cf76d1b100
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-15 22:27:51 +0530

    Updated Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,9 +9,6 @@ const Navbar = () => {
                 <a href="" className="logo">EK</a>
                 <input className="menu-btn" type="checkbox" id="menu-btn" />
                 <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-                <button id="no-web" className="resume1" target="_blank" rel="noopener noreferrer">
-                        <li> <a className="res" href="https://google.com"> Resume</a> </li>
-                </button>
                 <ul className="menu">
                     <li> <a href=""> About Me </a> </li>
                     <li> <a href=""> Skills</a> </li>
