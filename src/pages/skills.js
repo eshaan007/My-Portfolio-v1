c40ebab8af2b26437de3d90a493ce8c94373480a
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-15 23:25:32 +0530

    Updating Skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import i1 from '../images/android-studio.png';
+import '../styles/skills.scss';
 
 const Skills = () => {
     return(
         <div>
-            <h3> These are the technologies I have experience working with :  </h3>
-            <div>
-                <img src=""></img>
+            <h3 className="heading"> <a className="number-about">02. </a> My Skill - Set </h3>
+            <div className="logo-container">
+                <img className="logo" src={i1} alt="android"></img>
             </div>
         </div>
     )
