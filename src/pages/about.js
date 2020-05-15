83ac130fc9534fc28f0b2a791bfccdb92eb12eaf
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-15 23:45:31 +0530

    Editing Content

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
                 <h4 className="about-me-bold"> Hello! I am Eshaan Khurana. </h4>
                 <h4 className="about-me-bold"> I am a web developer leaning towards front end development and design.</h4>
                 <h4 className="about-me"> I am currently a 2nd year student at <a className="bold-blue">Indian Institute of Information Technology, Vadodara </a>pursuing B. Tech. in Information Technology.
-                I enjoy designing websites using multiple frameworks and tools provided by <a className="bold-yellow"> JS </a>.</h4>
+                I enjoy designing responsive websites using multiple frameworks and tools provided by <a className="bold-yellow"> JS </a>.</h4>
 
                 <h4 className="about-me"> My goal is to build <a className="bold-blue">pixel-perfect components</a> and practical projects which provide a rich user experience with <a className="bold-white">real world application.</a> </h4>
              
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -6,6 +6,8 @@ const Skills = () => {
     return(
         <div>
             <h3 className="heading"> <a className="number-about">02. </a> My Skill - Set </h3>
+            <h4 className="about-me-bold1"> These are the technologies I have experience working with :</h4>
+            
             <div className="logo-container">
                 <img className="logo" src={i1} alt="android"></img>
             </div>
