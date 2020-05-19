c17a3097d1a573c694abc36a1b31f57f5d132d7b
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-20 00:32:56 +0530

    Updated Certifications

--- src/pages/certifications.js
+++ src/pages/certifications.js
@@ -7,59 +7,70 @@ class Certifications extends Component {
     render() {
         return (
             <div id="cert-page">
-                <div className="contain1">
+
                 <Fade bottom>
                     <h3 className="heading-cert"> <a className="number-about"> 03. </a> My Certifications </h3>
                 </Fade>
 
-                    <div id="no-mob" className="logo-container">       
+                    <div className="logo-container">       
 
                         <div className="about-cert">
                         
-                            <Column flexGrow={1}>
+                            <Column className="col1" flexGrow={1}>
                                 <Row wrap vertical='center'>
-                                <Column flexGrow={1} horizontal='center'>
+                                <Column className="card" flexGrow={1} horizontal='center'>
                                 <h4 className="cert-name">Front-End Web Development with React</h4>
-                                <h4 className="cert-desc">By Coursera</h4>
+                                <h4 className="cert-desc">-Coursera</h4>
                                 </Column>
-                                <Column flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Front-End Web Development with React</h4>
-                                <h4 className="cert-desc">By Coursera</h4>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <h4 className="cert-name">The Complete 2020 Web Development Bootcamp</h4>
+                                <h4 className="cert-desc">-Udemy</h4>
                                 </Column>
                                 </Row>
                             </Column>
 
-                            <Column flexGrow={1}>
+                            <Column className="col2" flexGrow={1}>
                                 <Row wrap vertical='center'>
-                                <Column flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Front-End Web Development with React</h4>
-                                <h4 className="cert-desc">By Coursera</h4>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <h4 className="cert-name">Front-End Web UI, Frameworks and Tools : Bootstrap4</h4>
+                                <h4 className="cert-desc">-Coursera</h4>
                                 </Column>
-                                <Column flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Front-End Web Development with React</h4>
-                                <h4 className="cert-desc">By Coursera</h4>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <h4 className="cert-name">The Fundamentals of Digital Marketing</h4>
+                                <h4 className="cert-desc">-Google</h4>
                                 </Column>
                                 </Row>
                             </Column>
                         
-                            <Column flexGrow={1}>
+                            <Column className="col3" flexGrow={1}>
                                 <Row wrap vertical='center'>
-                                <Column flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Front-End Web Development with React</h4>
-                                <h4 className="cert-desc">By Coursera</h4>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <h4 className="cert-name">JAVA Skill IQ : Expert</h4>
+                                <h4 className="cert-desc">-PluralSight</h4>
                                 </Column>
-                                <Column flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Front-End Web Development with React</h4>
-                                <h4 className="cert-desc">By Coursera</h4>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <h4 className="cert-name">MySQL Fundamentals</h4>
+                                <h4 className="cert-desc">-PluralSight</h4>
+                                </Column>
+                                </Row>
+                            </Column>
+
+                            <Column className="col3" flexGrow={1}>
+                                <Row wrap vertical='center'>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <h4 className="cert-name">Completed Explore ML Track : Intermediate Level</h4>
+                                <h4 className="cert-desc">-Google</h4>
+                                </Column>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <h4 className="cert-name">Data Structures And Algorithms</h4>
+                                <h4 className="cert-desc">-GirlScript.tech</h4>
                                 </Column>
                                 </Row>
                             </Column>
                         
                         
                         </div>
-                
                     </div>
-                </div>
             </div>
         );
     }
