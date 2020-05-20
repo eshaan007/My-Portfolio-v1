42af622f3812e7b14fe6b8e3f3f762b38451d775
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-20 08:17:32 +0530

    Updated Certifications with links and accents

--- src/pages/certifications.js
+++ src/pages/certifications.js
@@ -15,59 +15,77 @@ class Certifications extends Component {
                     <div className="logo-container">       
 
                         <div className="about-cert">
-                        
+                            <Fade bottom>
                             <Column className="col1" flexGrow={1}>
                                 <Row wrap vertical='center'>
                                 <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Front-End Web Development with React</h4>
-                                <h4 className="cert-desc">-Coursera</h4>
+                                <a className="cert-name" href="https://coursera.org/share/8075cf9cd83b6feea0d7deecad649842" target="_blank" rel="noopener noreferrer" >Front-End Web Development with React</a>
+                                <a className="cert-desc">-Hong Kong University of Science and Technology </a>
+                                
                                 </Column>
                                 <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">The Complete 2020 Web Development Bootcamp</h4>
-                                <h4 className="cert-desc">-Udemy</h4>
+                                <a className="cert-name-udemy" target="_blank" rel="noopener noreferrer" href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-81c4e375-06f5-4fc2-9028-61ee4df7a8f1.pdf">The Complete 2020 Web Development Bootcamp</a>
+                                <a className="cert-desc">-Udemy</a>
                                 </Column>
                                 </Row>
                             </Column>
+                            </Fade>
 
+                            <Fade bottom>
                             <Column className="col2" flexGrow={1}>
                                 <Row wrap vertical='center'>
                                 <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Front-End Web UI, Frameworks and Tools : Bootstrap4</h4>
-                                <h4 className="cert-desc">-Coursera</h4>
+                                <a className="cert-name" href="https://www.coursera.org/account/accomplishments/certificate/KZD3YCXMXK25" target="_blank" rel="noopener noreferrer">Front-End Web UI, Frameworks and Tools : Bootstrap4</a>
+                                <a className="cert-desc">-Hong Kong University of Science and Technology</a>
                                 </Column>
                                 <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">The Fundamentals of Digital Marketing</h4>
-                                <h4 className="cert-desc">-Google</h4>
+                                <a className="cert-name-google" href="https://learndigital.withgoogle.com/digitalunlocked/validate-certificate-code" target="_blank" rel="noopener noreferrer">The Fundamentals of Digital Marketing</a>
+                                <a className="cert-desc">-Google</a>
                                 </Column>
                                 </Row>
                             </Column>
-                        
-                            <Column className="col3" flexGrow={1}>
-                                <Row wrap vertical='center'>
-                                <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">JAVA Skill IQ : Expert</h4>
-                                <h4 className="cert-desc">-PluralSight</h4>
-                                </Column>
-                                <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">MySQL Fundamentals</h4>
-                                <h4 className="cert-desc">-PluralSight</h4>
-                                </Column>
-                                </Row>
-                            </Column>
+                            </Fade>
 
+                            <Fade bottom>
                             <Column className="col3" flexGrow={1}>
                                 <Row wrap vertical='center'>
                                 <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Completed Explore ML Track : Intermediate Level</h4>
-                                <h4 className="cert-desc">-Google</h4>
+                                <a className="cert-name-pluralsight" href="https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fapp.pluralsight.com%2Fscore%2Fredir%2F3c330b34-6eb1-4374-a4ca-8cfbbaf13e8b%3Fec97ad1aba" target="_blank" rel="noopener noreferrer">JAVA Skill IQ : Expert</a>
+                                <a className="cert-desc">-PluralSight </a>
                                 </Column>
                                 <Column className="card" flexGrow={1} horizontal='center'>
-                                <h4 className="cert-name">Data Structures And Algorithms</h4>
-                                <h4 className="cert-desc">-GirlScript.tech</h4>
+                                <a className="cert-name-pluralsight" href="https://www.docdroid.net/wdNtKmG/mysqlfundamentals-pdf" target="_blank" rel="noopener noreferrer">MySQL Fundamentals</a>
+                                <a className="cert-desc">-PluralSight</a>
                                 </Column>
                                 </Row>
                             </Column>
+                            </Fade>
+
+                            <Fade bottom>
+                            <Column className="col3" flexGrow={1}>
+                                <Row wrap vertical='center'>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <a className="cert-name-google" href="https://google.qwiklabs.com/public_profiles/be9c5216-ec84-405c-8537-40e46f73ab26" target="_blank" rel="noopener noreferrer">Completed Explore ML Track : Intermediate Level</a>
+                                <a className="cert-desc">-Google</a>
+                                </Column>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <a className="cert-name-girlscript" href="https://imgur.com/a/MRDMJRi" target="_blank" rel="noopener noreferrer">Data Structures And Algorithms</a>
+                                <a className="cert-desc">-GirlScript.tech</a>
+                                </Column>
+                                </Row>
+                            </Column>
+                            </Fade>
                         
+                            <Fade bottom>
+                            <Column className="col1" flexGrow={1}>
+                                <Row wrap vertical='center'>
+                                <Column className="card" flexGrow={1} horizontal='center'>
+                                <a className="cert-name-udemy" href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-4c5df15f-8201-4a6d-958b-f19add0c9a54.pdf" target="_blank" rel="noopener noreferrer">Java Programming Masterclass</a>
+                                <a className="cert-desc">-Udemy</a>
+                                </Column>
+                                </Row>
+                            </Column>
+                            </Fade>
                         
                         </div>
                     </div>
