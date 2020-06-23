1c70df2eaa8eb0e2474d55fa4e43834e9f17da57
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-06-24 01:24:01 +0530

    Removed Warnings

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,21 +10,21 @@ class About extends Component {
         <div id="about-page">
             <div className="contain1">
             <Fade bottom>        
-                <h3 className="heading"> <a className="number-about">01. </a>About Me </h3>
+                <h3 className="heading"> <a className="number-about" href=" ">01. </a>About Me </h3>
                 <h4 className="about-me-bold"> Hello! I am Eshaan Khurana. </h4>
                 <h4 className="about-me-bold"> I am a developer leaning towards front end web development and design.</h4>
-                <h4 className="about-me"> I am currently a 2nd year student at <a className="bold-blue">Indian Institute of Information Technology, Vadodara </a>pursuing B. Tech. in Information Technology.
-                I enjoy designing responsive websites using multiple frameworks and tools provided by <a className="bold-yellow"> JS. </a></h4>
+                <h4 className="about-me"> I am currently a 2nd year student at <a className="bold-blue" href=" ">Indian Institute of Information Technology, Vadodara </a>pursuing B. Tech. in Information Technology.
+                I enjoy designing responsive websites using multiple frameworks and tools provided by <a className="bold-yellow" href=" "> JS. </a></h4>
 
-                <h4 className="about-me"> My goal is to build <a className="bold-blue">pixel-perfect components</a> and practical projects which provide a rich user experience with <a className="bold-white">real world application.</a> </h4>
+                <h4 className="about-me"> My goal is to build <a className="bold-blue" href=" ">pixel-perfect components</a> and practical projects which provide a rich user experience with <a className="bold-white" href=" ">real world application.</a> </h4>
 
                 {/* <h4 id="no-mob" className="about-me"> I aspire toward a career that will allow me to channelize my creativity towards building interfaces which are not only <a className="bold-white">mobile friendly</a> but also user friendly providing engaging experiences.
                 I'm a keen learner who is always <a className="bold-white">looking for opportunities to absorb and work </a>with those who are willing to share their knowledge and experience.</h4>
                  */}
-                <h4 className="about-me">  <a className="bold-white"> When not in front of a computer,</a> you will probably find me playing my
-                    <a className="bold-brown"> 🎸, </a> relishing
+                <h4 className="about-me">  <a className="bold-white" href=" "> When not in front of a computer,</a> you will probably find me playing my
+                     🎸, relishing
                     🎵🎧, watching ⚽, blogging👨‍💻 or enjoying 
-                    <a className="bold-green"> </a> 🎮 sessions  with my friends.</h4>
+                    <a className="bold-green" href=" "> </a> 🎮 sessions  with my friends.</h4>
             </Fade>    
             </div>
 
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -34,7 +34,7 @@ class Skills extends Component {
     return(
         <div id="skill-top">
             <Fade bottom>
-            <h3 className="heading"> <a className="number-about"> 02. </a> My Skill-Set </h3>
+            <h3 className="heading"> <a className="number-about" href=" "> 02. </a> My Skill-Set </h3>
             <h4 className="about-me-bold1"> Some technologies I have experience working with :</h4>        
             </Fade>
 
