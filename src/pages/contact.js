d0fc9ccb1f88db8175b737c93ef91c5dd637ae87
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-07-05 01:07:47 +0530

    Adding Contact logos and links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,11 @@
 import React, { Component} from 'react';
 import '../styles/contact.scss';
 import Fade from 'react-reveal/Fade';
+import github from '../images/github-logo.png';
+import gmail from '../images/gmail.png';
+import linkedin from '../images/linkedin.png';
+import medium from '../images/medium.png';
+import twitter from '../images/twitter.png';
 
 class Contact extends Component {
     render() {
@@ -8,7 +13,13 @@ class Contact extends Component {
         <Fade bottom>
             <div>
                 <h2 className="cent-line"> I am available for any collabs and new ideas here : </h2>
-                
+                <div className="logos">       
+                    <a href="https://github.com/eshaan007/" target="_blank" rel="noopener noreferrer"><img className="logo" src={github} alt="github"></img></a>
+                    <a href="mailto:eshaany2k@gmail.com" target="_blank" rel="noopener noreferrer"><img className="logo" src={gmail} alt="gmail"></img></a>
+                    <a href="https://linkedin.com/in/eshaan-khurana/" target="_blank" rel="noopener noreferrer"><img className="logo" src={linkedin} alt="linkedin"></img></a>
+                    <a href="https://medium.com/@eshaany2k/" target="_blank" rel="noopener noreferrer"><img className="logo" src={medium} alt="medium"></img></a>
+                    <a href="https://twitter.com/eshaaaan007" target="_blank" rel="noopener noreferrer"><img className="logo" src={twitter} alt="twitter"></img></a>
+               </div>
             </div>
         </Fade>
     );
