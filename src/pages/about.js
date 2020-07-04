85e470ed107654aa8204f83f33f3b0745584c83f
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-07-04 23:13:56 +0530

    Adding Contact Section

--- src/pages/about.js
+++ src/pages/about.js
@@ -37,4 +37,4 @@ class About extends Component {
     }
 }
 
-export default About
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react';
+import '../styles/contact.scss';
+import Fade from 'react-reveal/Fade';
+
+class Contact extends Component {
+    render() {
+    return(
+        <Fade bottom>
+            <div>
+
+            </div>
+        </Fade>
+    );
+    }
+}
+
+export default Contact;
