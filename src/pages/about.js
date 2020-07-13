ab529215cd7417053f0c5cf36da51f1c0c06efe1
Eshaan Khurana <eshaany2k@gmail.com>
2020-07-13 11:21:59 +0530

    Edited Education Year

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,7 @@ class About extends Component {
                 <h3 className="heading"> <a className="number-about" href=" ">01. </a>About Me </h3>
                 <h4 className="about-me-bold"> Hello! I am Eshaan Khurana. </h4>
                 <h4 className="about-me-bold"> I am a developer leaning towards front end web development and design..</h4>
-                <h4 className="about-me"> I am currently a 2nd year student at <a className="bold-blue" href=" ">Indian Institute of Information Technology, Vadodara </a>pursuing B. Tech. in Information Technology.
+                <h4 className="about-me"> I am currently a 3rd year Undergraduate (2018-22) at <a className="bold-blue" href=" ">Indian Institute of Information Technology, Vadodara </a>pursuing B. Tech. in Information Technology.
                 I enjoy designing responsive websites using multiple frameworks and tools provided by <a className="bold-yellow" href=" "> JS. </a></h4>
 
                 <h4 className="about-me"> My goal is to build <a className="bold-blue" href=" ">pixel-perfect components</a> and practical projects which provide a rich user experience with <a className="bold-white" href=" ">real world application.</a> </h4>
