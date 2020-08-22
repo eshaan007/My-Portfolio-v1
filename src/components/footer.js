82d370c85af376304daac38b2ec1c5833d8c1e54
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-08-22 17:26:16 +0530

    Centre align correction

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,7 @@ import Flip from 'react-reveal/Flip';
 
  class Footer extends Component{
     render() {
-    return(
-        // <a  href="https://www.netlify.com">
-        //     <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
-        // </a>
-
-        //BEWARE! This link will lead you to a geek's thoughts.
-        
+    return(   
         <div className="cont">
             <Flip cascade>
                 <h4 className="intro1"> Designed and Developed by</h4>
--- src/pages/about.js
+++ src/pages/about.js
@@ -18,9 +18,6 @@ class About extends Component {
 
                 <h4 className="about-me"> My goal is to build <a className="bold-blue" href=" ">pixel-perfect components</a> and practical projects which provide a rich user experience with <a className="bold-white" href=" ">real world application.</a> </h4>
 
-                {/* <h4 id="no-mob" className="about-me"> I aspire toward a career that will allow me to channelize my creativity towards building interfaces which are not only <a className="bold-white">mobile friendly</a> but also user friendly providing engaging experiences.
-                I'm a keen learner who is always <a className="bold-white">looking for opportunities to absorb and work </a>with those who are willing to share their knowledge and experience.</h4>
-                 */}
                 <h4 className="about-me">  <a className="bold-white" href=" "> When not in front of a computer,</a> you will probably find me playing my
                      🎸, relishing
                     🎵🎧, watching ⚽, blogging👨‍💻 or enjoying 
