7cc446fa265818bc6897bc907d131c9cd16d70fd
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-08-25 17:47:19 +0530

    Contact Icons Responsive

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,11 +14,11 @@ class Contact extends Component {
             <div id="Contact-section">
                 <h2 className="cent-line"> I am available for any new collabs, opportunities and ideas here : </h2>
                 <div className="logos">  
-                    <a className="sign" href="https://github.com/eshaan007/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="3x" color="#2bbc8a"/></a> {   }
-                    <a className="sign" href="mailto:eshaany2k@gmail.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} size="3x" color="#2bbc8a"/></a> {   }
-                    <a className="sign" href="https://linkedin.com/in/eshaan-khurana/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} size="3x" color="#2bbc8a"/></a> {    }
-                    <a className="sign" href="https://medium.com/@eshaany2k/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faMediumM} size="3x" color="#2bbc8a"/></a> {    }
-                    <a className="sign" href="https://twitter.com/eshaaaan007" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} size="3x" color="#2bbc8a"/></a> {    }
+                    <a className="sign" href="https://github.com/eshaan007/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fontHai" icon={faGithub} size="3x" color="#2bbc8a"/></a> {   }
+                    <a className="sign" href="mailto:eshaany2k@gmail.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fontHai" icon={faEnvelope} size="3x" color="#2bbc8a"/></a> {   }
+                    <a className="sign" href="https://linkedin.com/in/eshaan-khurana/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fontHai" icon={faLinkedinIn} size="3x" color="#2bbc8a"/></a> {    }
+                    <a className="sign" href="https://medium.com/@eshaany2k/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fontHai" icon={faMediumM} size="3x" color="#2bbc8a"/></a> {    }
+                    <a className="sign" href="https://twitter.com/eshaaaan007" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fontHai" icon={faTwitter} size="3x" color="#2bbc8a"/></a> {    }
                </div>
             </div>
         </Fade>
